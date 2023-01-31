c7748b3971618c77e30d8d7c388041aedc93b2fd
Ryan <ryanbairdchilders@gmail.com>
2023-01-31 10:13:34 -0800

    started adding get and post apps

--- server.js
+++ server.js
@@ -5,9 +5,13 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 // making public folder available to requests for clients
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 // add body parsing middleware
+
+app.use(express());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // GET * should return the index.html file.
 app.get("/", (req, res) => {
@@ -25,8 +29,16 @@ app.get("/notes", (req, res) => {
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 
+app.get("/api/notes", )
+
 
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+
+app.post("/api/notes", (req, res) => {
+    
+})
+
+
 app.listen(PORT, () => {
     console.log("server listening on PORT 3001");
 } )
